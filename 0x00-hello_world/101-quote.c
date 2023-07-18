25bbfce72df23709bc646df0a06eadffc15299b0
Technita <raagaleugwuru@gmail.com>
2023-07-17 18:18:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<unistd.h>
 
+
 /**
  * main - main function
  *
@@ -8,7 +9,9 @@
  */
 
 int main(void)
+
 {
-	Write(2, "and that piece of art is useful\"-Dora Korpar,2015-10-19\n", 59);
+	Write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
